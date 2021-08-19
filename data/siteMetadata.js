93d6b1ca0be830aab264e321b155980f5fe61d72
Umer Farooq <umer@spaceship.com.au>
2021-08-19 15:46:11 +1000

    add better social share icon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/umer-memoji.png',
   email: 'omer.farooque@gmail.com',
   github: 'https://github.com/mumerfarooq',
   twitter: 'https://twitter.com/gulshaniya/',
