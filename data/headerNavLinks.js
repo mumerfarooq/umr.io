ea2b865a8dba267d2ae34e283220ec972c88c0f8
Umer Farooq <umer@spaceship.com.au>
2021-08-21 16:17:04 +1000

    rename header link to blog

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
   { href: '/books', title: 'Bookshelf' },
-  { href: '/blog', title: 'Musings' },
+  { href: '/blog', title: 'Blog' },
 ]
 
 export default headerNavLinks
