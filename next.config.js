7b66a71a6dd0095314d27b0b987ec3e19e0422c0
Umer <umer@ceri.app>
2022-03-15 12:49:18 +1100

    quick hack for goat counter

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app gc.zoo.at;
   style-src 'self' 'unsafe-inline' '';
   img-src * blob: data:;
   media-src 'none';
