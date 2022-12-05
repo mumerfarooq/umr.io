906a52ae252e647d3c1869aad8474bc6d4d6cce6
Umer <omer.farooque@gmail.com>
2022-12-05 21:41:30 +1100

    hide pitter

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/about', title: 'About' },
   { href: '/books', title: 'Bookshelf' },
   { href: '/blog', title: 'Blog' },
-  { href: '/pitter', title: 'Pitter' },
+  //{ href: '/pitter', title: 'Pitter' },
 ]
 
 export default headerNavLinks
