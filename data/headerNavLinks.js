30f3ae6cb11e210e6c899cb608aec24a2f606563
Umer <omer.farooque@gmail.com>
2024-09-22 16:25:41 -0700

    add shop

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/about', title: 'About' },
   { href: '/books', title: 'Bookshelf' },
   { href: '/blog', title: 'Blog' },
-  //{ href: '/pitter', title: 'Pitter' },
+  { href: 'https://shop.umr.io', title: 'Shop' },
 ]
 
 export default headerNavLinks
